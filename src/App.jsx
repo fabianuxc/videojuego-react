b247f72397127be6443502f2a282412e71adb035
Fabián Gómez Campo <fgomezc@uxcale.com>
2024-07-24 16:51:50 +0200

    added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,35 @@
 import { useState } from 'react'
-import './App.css'
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinnerFrom, checkEndGame } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
+import { Board } from './components/Board'
+import { saveToStorage, resetStorage } from './logic/storage'
 
 
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
-
-const Square = ({ children, updateBoard, index, isSelected }) => {
-
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
 
 function App() {
 
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    console.log(turnFromStorage)
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
-  const checkWinner = (boardToCheck) => {
-    for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a]
-      }
-    }
-
-    return null
-  }
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetStorage()
   }
   
-  const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square !== null)
-  }
-
   const updateBoard = (index) => {
     // avoid replacing a filled square or if there's a winner
     if (board[index] || winner) return
@@ -77,8 +43,14 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
+    // save game
+    saveToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+
     // check for winners
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       setWinner(newWinner)
     } else if (checkEndGame(newBoard)) {
@@ -92,51 +64,14 @@ function App() {
       <h1>Tic tac toe</h1>
       <button onClick={resetGame}>Game reset</button>
       <section className='game'>
-        {
-          board.map((square, index) => {
-            return (
-              <Square
-                key={index}
-                index={index}
-                updateBoard={updateBoard}>
-                {square}
-
-              </Square>
-            )
-          })
-        }
+        <Board board={board} updateBoard={updateBoard} />
       </section>
       <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
-      {
-        winner !== null && (
-          <section className='winner'>
-            <div className="text">
-              <h2>
-                {
-                  winner === false
-                    ? 'Tie'
-                    : 'Won'
-                }
-              </h2>
-              <header className='win'>
-                {winner && <Square>{winner}</Square>}
-              </header>
-
-              <footer>
-                <button onClick={resetGame}>Restart</button>
-              </footer>
-
-            </div>
-
-
-          </section>
-
-        )
-      }
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
   )
 }
