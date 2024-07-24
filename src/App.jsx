735f9b372ad9c8fb776a68dce9fed0735c9c6a52
Fabián Gómez Campo <fgomezc@uxcale.com>
2024-07-24 10:41:09 +0200

    Board drawn

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
 
+
+
+const Square = ({ children, updateBoard, index }) => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
+function App() {
+
+  const [board, setBoard] = useState(['x', 'x', 'x', 'o', 'o', 'x', 'x', 'o', 'x'])
+  
+  return (
+    <main className='board'>
+      <h1>Tic tac toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}>
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
   )
 }
 
